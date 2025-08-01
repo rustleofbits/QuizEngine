df5d8dc81b6a84ac4e6bd88a56d7c77df764f5c0
rustleofbits <rustleofbits@gmail.com>
2025-08-01 21:16:42 +0500

    minor refactoring

--- QuizEngineTests/GameTest.swift
+++ QuizEngineTests/GameTest.swift
@@ -12,22 +12,24 @@ class GameTest: XCTestCase {
     let router = RouterSpy()
     var game: Game<String, String, RouterSpy>!
     
-    func test_startGame_answerZeroOutOfTwoCorrectly_scoresZero() {
+    override func setUp() {
+        super.setUp()
         game = startGame(questions: ["Q1", "Q2"], router: router, correctAnswers: ["Q1": "A1", "Q2": "A2"])
+    }
+    
+    func test_startGame_answerZeroOutOfTwoCorrectly_scoresZero() {
         router.answerCallback("wrong")
         router.answerCallback("wrong")
         XCTAssertEqual(router.routedResult?.score, 0)
     }
     
     func test_startGame_answerOneOutOfTwoCorrectly_scoresOne() {
-        game = startGame(questions: ["Q1", "Q2"], router: router, correctAnswers: ["Q1": "A1", "Q2": "A2"])
         router.answerCallback("A1")
         router.answerCallback("wrong")
         XCTAssertEqual(router.routedResult?.score, 1)
     }
     
     func test_startGame_answerTwoOutOfTwoCorrectly_scoresTwo() {
-        game = startGame(questions: ["Q1", "Q2"], router: router, correctAnswers: ["Q1": "A1", "Q2": "A2"])
         router.answerCallback("A1")
         router.answerCallback("A2")
         XCTAssertEqual(router.routedResult?.score, 2)
